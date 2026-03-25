beb49f2768158005debc2d0d6ae2f80a78d87ebe
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-25 11:51:37 +0900

    feat: 과일 목록 렌더링 및 장바구니 담기 기능 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 // TODO 5: props를 받아서 과일 목록을 렌더링하세요
 //         - fruits: 전체 과일 배열
 //         - onAddToCart: 장바구니 추가 함수
-const Home = () => {
+const Home = ({ fruits, onAddToCart }) => {
   return (
     <div style={{ padding: "20px" }}>
       <h2>과일 목록</h2>
       {/* TODO: fruits 배열을 map으로 순회하며 과일 카드를 렌더링하세요 */}
       {/* 각 카드에는 이모지, 이름, 가격, "장바구니 담기" 버튼이 있어야 합니다 */}
-      <p>여기에 과일 목록을 렌더링하세요</p>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            <p>{fruit.emoji}</p>
+            <p>{fruit.name}</p>
+            <p>{fruit.price}</p>
+            <button type="button" onClick={() => onAddToCart(fruit)}>
+              장바구니 담기
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
