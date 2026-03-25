8d01469e097265102b91f76bd9e0f9a2e4d197ab
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-25 11:51:52 +0900

    feat: 장바구니 목록 조회 및 아이템 제거 기능 구현

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,35 @@
 // TODO 6: props를 받아서 장바구니 목록을 렌더링하세요
 //         - cart: 장바구니에 담긴 과일 배열
 //         - onRemoveFromCart: 장바구니 제거 함수
-const About = () => {
+const About = ({ cart, onRemoveFromCart }) => {
+  if (cart.length === 0) {
+    return (
+      <div style={{ padding: "20px" }}>
+        <h2>장바구니</h2>
+        <p>장바구니가 비어 있습니다</p>
+      </div>
+    );
+  }
+
   return (
     <div style={{ padding: "20px" }}>
       <h2>장바구니</h2>
       {/* TODO: cart 배열을 map으로 순회하며 장바구니 아이템을 렌더링하세요 */}
       {/* 각 아이템에는 이모지, 이름, 가격, "제거" 버튼이 있어야 합니다 */}
       {/* 장바구니가 비어있으면 "장바구니가 비어있습니다" 메시지를 보여주세요 */}
-      <p>여기에 장바구니 목록을 렌더링하세요</p>
+
+      <ul>
+        {cart.map((item) => (
+          <li key={item.id}>
+            <p>{item.emoji}</p>
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+            <button type="button" onClick={() => onRemoveFromCart(item.id)}>
+              제거
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
